23bea8cdcba5e1931e9bf47d83c3a2533dffb4f3
Mariagoretti <mariakimanigoreti@gmail.com>
2023-02-02 09:30:10 +0300

    datatypes

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+import java.security.KeyStore.TrustedCertificateEntry
+
+fun main(args: Array<String>) {
+    var fulname = "Mariagoretti Kimani"
+    var age = 12
+    var  phoneNumber = 712345678
+    var Weight = 50
+    var Citizen: Boolean = true
+    println(fulname)
+    println("Kenyan")
+
+
+}
